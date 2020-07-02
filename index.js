531a5284f945cc0e41f55cae665833b5294886de
toanlehcm <thanhtoanhcm1602@gmail.com>
2020-07-03 00:29:04 +0700

    update cat class

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var Dog = require('./dog');
 var Cat = require('./cat'); 
+var Mouse = require('./mouse');
 
-var dog = new Dog('Tom');
-var tom = new Cat();
+// var dog = new Dog('Tom');
+// var tom = new Cat();
+// dog.eat(tom);
+// dog.sayHi();
 
-dog.eat(tom);
-dog.sayHi();
-// test login with cache
-// test second login
-// test git pull
+var cat = new Cat();
+var mouse = new Mouse('Mickey');
+var dog = new Dog('bobabob');
+
+try {
+    cat.eat(dog);
+} catch (e) {
+   console.log('error while cat eating a dog'); 
+}
+
+cat.eat(mouse);
+console.log(cat);
